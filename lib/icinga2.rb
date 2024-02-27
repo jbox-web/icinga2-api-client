22ec2c0315dc2d4b33412dd26c1466dd73149706
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-02-27 03:54:43 +0100

    Fix inflector for version

--- lib/icinga2.rb
+++ lib/icinga2.rb
@@ -7,6 +7,7 @@ require 'active_support/core_ext/object/to_query'
 require 'zeitwerk'
 loader = Zeitwerk::Loader.for_gem
 loader.inflector.inflect 'api' => 'API'
+loader.inflector.inflect 'version' => 'VERSION'
 loader.setup
 
 module Icinga2
