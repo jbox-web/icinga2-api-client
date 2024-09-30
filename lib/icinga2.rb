4eafc32a54f5fee5983b11c064c94cc630dde875
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-09-30 03:03:35 +0200

    Coding style

--- lib/icinga2.rb
+++ lib/icinga2.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
+# require ruby dependencies
 require 'json'
+
+# require external dependencies
 require 'faraday'
 require 'active_support/core_ext/object/to_query'
-
 require 'zeitwerk'
-loader = Zeitwerk::Loader.for_gem
-loader.inflector.inflect 'api' => 'API'
-loader.inflector.inflect 'version' => 'VERSION'
-loader.setup
+
+# load zeitwerk
+Zeitwerk::Loader.for_gem.tap do |loader|
+  loader.inflector.inflect 'api' => 'API'
+  loader.inflector.inflect 'version' => 'VERSION'
+  loader.setup
+end
 
 module Icinga2
 end
